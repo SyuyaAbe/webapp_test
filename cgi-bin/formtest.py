c32a0eef63334733b9a6f4bab0f5da884d72e1ca
S.A <s.a0107hokkaidou.fuh@gmail.com>
2021-01-15 23:40:11 +0900

    Add files via upload

--- cgi-bin/formtest.py
+++ cgi-bin/formtest.py
@@ -1,31 +1,12 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
 import cgi
-form = cgi.FieldStorage() # cgiオブジェクト作成
-v1 = form.getfirst('value1') # nameがvalue1の値を取得
-v2 = form.getfirst('value2') # nameがvalue2の値を取得
-#入力値が数字ならその積を返す関数
-def times(a, b):
-    try:
-        a=int(a)
-        b=int(b)
-        return str(a*b)
-    except ValueError:
-        return('数値じゃないので計算できません(>_<)')
+import cgitb
+cgitb.enable()
 
-# ブラウザに戻すHTMLのデータ
 print("Content-Type: text/html")
-print()
-htmlText = '''
-<!DOCTYPE html>
-<html>
-    <head><meta charset="utf-8" /></head>
-<body bgcolor="lightyellow">
-    <h1>成功です．</h1>
-    <p>入力値の積は&nbsp; %s<br/></p>
-    <hr/>
-    <a href="../form1.html">戻る</a>　
-</body>
-</html>
-'''%(times(v1,v2)) # 入力値の積を%sの箇所に埋める
-#print(( htmlText.encode("cp932", 'ignore').decode('cp932') ))
+print()   
+
+form = cgi.FieldStorage()
+
+for key in form:
+    value = form[key].value
+    print('<p>%s: %s</p>' % (key, value))
